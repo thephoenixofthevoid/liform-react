99f3583f2f525925ff811948ba73e3e0e89cc98a
Phoenix The Fallen <thephoenixofthevoid@gmail.com>
2019-08-06 07:17:07 +0300

    compareByOrder

--- src/renderFields.js
+++ src/renderFields.js
@@ -1,26 +1,23 @@
 import renderField from "./renderField";
 
 export const isRequired = (schema, fieldName) => {
-  if (!schema.required) {
-    return false;
-  }
+  if (!schema.required) return false;
   return schema.required.indexOf(fieldName) !== -1;
 };
+
+function compareByOrder(a, b) {
+  if (a.propertyOrder > b.propertyOrder) return 1;
+  if (a.propertyOrder < b.propertyOrder) return -1;
+  return 0;
+}
+
 
 const renderFields = (schema, theme, prefix = null, context = {}) => {
   let props = [];
   for (let i in schema.properties) {
     props.push({ prop: i, propertyOrder: schema.properties[i].propertyOrder });
   }
-  props = props.sort((a, b) => {
-    if (a.propertyOrder > b.propertyOrder) {
-      return 1;
-    } else if (a.propertyOrder < b.propertyOrder) {
-      return -1;
-    } else {
-      return 0;
-    }
-  });
+  props = props.sort(compareByOrder);
   return props.map(item => {
     const name = item.prop;
     const field = schema.properties[name];
